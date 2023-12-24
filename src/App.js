9fdf29db106829eb33f3463a44598db6245e0023
Soly-M-27 <solymar227@icloud.com>
2023-12-24 18:37:23 -0400

    Adding React + Firebase configuration files. Created LogIn and SignUp pages and successfully connected to Firebase authentication console

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
+import Home from './pages/home/Home';
+import Signup from './pages/signup/Signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB8xSSg4r_Vg5gp2pfUnQRKgoVQcpAJ74M",
+    authDomain: "ar-rolodex.firebaseapp.com",
+    projectId: "ar-rolodex",
+    storageBucket: "ar-rolodex.appspot.com",
+    messagingSenderId: "958075539111",
+    appId: "1:958075539111:web:4b42691824d326cf855582"
+  };
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+export { projectFirestore, projectAuth } 
